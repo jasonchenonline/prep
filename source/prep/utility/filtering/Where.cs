be61ed415620e57457e2bd2f6b96a528428e4f62
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-09-16 17:16:47 -0600

    Updated

---
+++ source/prep/utility/filtering/Where.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace prep.utility.filtering
+{
+  public delegate TPropertyValueType IGetTheValueOfAProperty<TItemToGetValueFrom, TPropertyValueType>(
+    TItemToGetValueFrom item_to_get_value_from);
+
+  public class Where<TItemToMatch>
+  {
+    public static MatchFactory<TItemToMatch, TPropertyType> has_a<TPropertyType>(
+      IGetTheValueOfAProperty<TItemToMatch, TPropertyType> property_accessor)
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+  public class MatchFactory<TItemToMatch, TPropertyType>
+  {
+    public IMatchA<TItemToMatch> equal_to(TPropertyType value)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
