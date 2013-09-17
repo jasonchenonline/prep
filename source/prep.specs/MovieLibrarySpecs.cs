d3410d03566682e233938cbf2957935920686290
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-09-16 18:00:36 -0600

    Updated

--- source/prep.specs/MovieLibrarySpecs.cs
+++ source/prep.specs/MovieLibrarySpecs.cs
@@ -205,10 +205,10 @@ namespace prep.specs
 
       It should_be_able_to_find_all_movies_published_by_pixar_or_disney = () =>
       {
-        var condition = Movie.is_published_by(ProductionStudio.Pixar)
-                             .or(Movie.is_published_by(ProductionStudio.Disney));
+        var criteria = Where<Movie>.has_a(x => x.production_studio)
+                                   .equal_to_any(ProductionStudio.Pixar, ProductionStudio.Disney);
 
-            var results = sut.all_movies().all_matching(condition);
+        var results = sut.all_movies().all_matching(criteria);
 
         results.ShouldContainOnly(a_bugs_life, pirates_of_the_carribean, cars);
       };
--- source/prep/utility/filtering/Where.cs
+++ source/prep/utility/filtering/Where.cs
@@ -1,20 +1,28 @@
-﻿using System;
+﻿using prep.collections;
 
 namespace prep.utility.filtering
 {
   public delegate TPropertyValueType IGetTheValueOfAProperty<TItemToGetValueFrom, TPropertyValueType>(
     TItemToGetValueFrom item_to_get_value_from);
 
-    public TPropertyValueType ( )
+   /// <summary>
+   /// Where class is a gateway to a specification factory
+   /// </summary>
+   /// <typeparam name="TItemToMatch"></typeparam>
   public class Where<TItemToMatch>
   {
     public static MatchFactory<TItemToMatch, TPropertyType> has_a<TPropertyType>(
       IGetTheValueOfAProperty<TItemToMatch, TPropertyType> property_accessor)
     {
-        return new MatchFactory<TItemToMatch, TPropertyType>(property_accessor);
+      return new MatchFactory<TItemToMatch, TPropertyType>(property_accessor);
     }
   }
 
+  /// <summary>
+  /// Factory pattern implementation - Creates specifications
+  /// </summary>
+  /// <typeparam name="TItemToMatch"></typeparam>
+  /// <typeparam name="TPropertyType"></typeparam>
   public class MatchFactory<TItemToMatch, TPropertyType>
   {
     IGetTheValueOfAProperty<TItemToMatch, TPropertyType> property_accessor;
@@ -26,7 +34,12 @@ namespace prep.utility.filtering
 
     public IMatchA<TItemToMatch> equal_to(TPropertyType value)
     {
-        return new AnonymousMatch<TItemToMatch>( x=>  ).matches(value);
+      return new AnonymousMatch<TItemToMatch>(x => property_accessor(x).Equals(value));
+    }
+
+    public IMatchA<TItemToMatch> equal_to_any(params TPropertyType[] values)
+    {
+      throw new System.NotImplementedException();
     }
   }
 }
