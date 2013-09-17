fd4a893808b5c7852359f4333e5a78dbbf62c884
jasonchenonline <jxc_2003sg@yahoo.com>
2013-09-17 10:37:46 +0800

    Pushing new changes

--- source/prep/utility/filtering/MatchFactory.cs
+++ source/prep/utility/filtering/MatchFactory.cs
@@ -24,7 +24,13 @@ namespace prep.utility.filtering
 
     public IMatchA<TItemToMatch> equal_to_any(params TPropertyType[] values)
     {
-      return new AnonymousMatch<TItemToMatch>(x =>
+      //return new AnonymousMatch<TItemToMatch>(x =>
+      //{
+      //  var property_value = property_accessor(x);
+      //  var all_possible_values = new List<TPropertyType>(values);
+      //  return all_possible_values.Contains(property_value);
+      //});
+      return MyMatchFactory<TItemToMatch>.getMatch(x =>
       {
         var property_value = property_accessor(x);
         var all_possible_values = new List<TPropertyType>(values);
---
+++ source/prep/utility/filtering/MyMatchFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prep.utility.filtering
+{
+    class  MyMatchFactory <ItemToMatch> 
+    {
+        //public delegate bool Condition<T>(T item);
+        public static AnonymousMatch<ItemToMatch> getMatch(Condition<ItemToMatch> condition)
+        {
+            return new AnonymousMatch<ItemToMatch>(condition);
+        }
+        
+    }
+}
