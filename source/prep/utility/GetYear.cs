159fc08c2d2b4803e35585c21c002196e868a46a
jasonchenonline <jxc_2003sg@yahoo.com>
2013-09-17 12:13:25 +0800

    Pushing new changes

---
+++ source/prep/utility/GetYear.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using prep.utility.filtering;
+
+namespace prep.utility
+{
+    class GetYear
+    {
+        public static int Year(DateTime dateTime)
+        {
+            return dateTime.Year;
+        }
+
+        public static IMatchA<TItemToMatch> greater_than<TItemToMatch>(this MatchCreationExtensionPoint<TItemToMatch, DateTime> extension_point, DateTime value) where TPropertyType : IComparable<TPropertyType>
+        {
+            return create_match(extension_point, new GreaterThan<DateTime>(value));
+        }
+
+
+    }
+}   
