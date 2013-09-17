4f0caa24e56eb0e7d584e1d1d0dd509194c55186
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-09-16 22:19:45 -0600

    Updated

---
+++ source/prep/utility/filtering/DateMatchCreationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace prep.utility.filtering
+{
+  public static class DateMatchCreationExtensions
+  {
+    public static IMatchA<TItemToMatch> greater_than<TItemToMatch>(
+      this MatchCreationExtensionPoint<TItemToMatch, DateTime> extension_point, int value)
+    {
+      return extension_point.create_match(Where<DateTime>.has_a(x => x.Year).greater_than(value));
+    }
+  }
+}
